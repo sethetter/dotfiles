8af618049bd39b6a1c873e8029b6055b9517cdea
Seth Etter <sethetter@gmail.com>
2019-06-24 15:10:27 -0500

    Back to VIM\!

--- nixpkgs/home.nix
+++ nixpkgs/home.nix
@@ -7,12 +7,12 @@ with import <home-manager/modules/lib/dag.nix> { inherit lib; };
 
 {
   home.packages = [
+    pkgs.vim
+    pkgs.tmux
     pkgs.ripgrep
     pkgs.jq
-    pkgs.emacs
-    pkgs.vim
-    pkgs.firefox
     pkgs.fzf
+    pkgs.firefox
   ];
 
   # Let Home Manager install and manage itself.
@@ -32,19 +32,6 @@ with import <home-manager/modules/lib/dag.nix> { inherit lib; };
   home.keyboard.options = ["ctrl:nocaps"];
 
   home.file = {
-    # Spacemacs
-    # TODO: use spacemacs2nix (github.com/puffnfresh/nix-files/tree/master/spacemacs)
-    # ".emacs.d" = {
-    #   source = pkgs.fetchFromGitHub {
-    #     owner = "syl20bnr";
-    #     repo = "spacemacs";
-    #     rev = "8c0b8c3";
-    #     sha256 = "1af73ls0znbpx73ym9cx22yc6q1dvv375l493ccilg2xpjsgam6w";
-    #   };
-    #   recursive = true;
-    # };
-    ".spacemacs".source = ~/dotfiles/spacemacs;
-
     # Fish + omf
     # ".omf" = {
     #   source = pkgs.fetchFromGitHub {
@@ -60,11 +47,10 @@ with import <home-manager/modules/lib/dag.nix> { inherit lib; };
     #   recursive = true;
     # };
     ".config/fish/config.fish".source = ~/dotfiles/config.fish;
+    ".vimrc".source = ~/dotfiles/vimrc;
   };
 
   # home.activation.installAndLinkNonNixManaged = dagEntryAfter [ "writeBoundary" ] ''
-  #   curl -L https://get.oh-my.fish | fish
-  #   git clone git@github.com:syl20bnr/spacemacs.git ~/.emacs.d
   #   rm -rf $HOME/.config/omf
   #   ln -sf $HOME/dotfiles/omf $HOME/.config/omf
   # '';
