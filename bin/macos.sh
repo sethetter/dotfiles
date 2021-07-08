69c8797fddfd92b0151bed4670d3bdba3087bf31
Seth Etter <sethetter@gmail.com>
2021-07-08 12:44:01 -0500

    Install Plug for neovim in macos setup

--- bin/macos.sh
+++ bin/macos.sh
@@ -5,6 +5,7 @@ brew install \
   tmux \
   vim \
   vifm \
+  neovim \
   fish \
   fzf \
   go \
@@ -16,3 +17,6 @@ brew install \
   nmap \
   bat \
   ripgrep
+
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
