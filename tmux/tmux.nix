43c5422ed0714a84f8ae262d8e74e072ee70ba70
Seth Etter <sethetter@gmail.com>
2021-08-23 20:45:47 -0500

    Tmux fixes

--- tmux/tmux.nix
+++ tmux/tmux.nix
@@ -1,15 +1,18 @@
 { pkgs }:
 {
-    enable = true;
-    prefix = "C-a";
-    terminal = "xterm-256color";
-    extraConfig = (builtins.readFile ./tmux.conf);
-    plugins = with pkgs.tmuxPlugins; [
-      tmux-colors-solarized
-      copycat
-      sensible
-      tmux-fzf
-      fzf-tmux-url
-      sessionist
-    ];
-  }
+  enable = true;
+  prefix = "C-a";
+  terminal = "xterm-256color";
+  extraConfig = (builtins.readFile ./tmux.conf);
+  plugins = with pkgs.tmuxPlugins; [
+    {
+      plugin = tmux-colors-solarized;
+      extraConfig = "set -g @colors-solarized 'light'";
+    }
+    copycat
+    sensible
+    tmux-fzf
+    fzf-tmux-url
+    sessionist
+  ];
+}
