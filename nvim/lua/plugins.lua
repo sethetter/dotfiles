96b2f84187273c87d3e26ff278698802b83e2a32
Seth Etter <sethetter@gmail.com>
2021-12-16 15:44:24 -0600

    lol jk

---
+++ nvim/lua/plugins.lua
@@ -0,0 +1,46 @@
+return require('packer').startup(function(use)
+  use 'wbthomason/packer.nvim'
+
+  use 'vijaymarupudi/nvim-fzf'
+  use { 'ibhagwan/fzf-lua',
+    requires = {
+      'vijaymarupudi/nvim-fzf',
+      'kyazdani42/nvim-web-devicons'
+    }
+  }
+
+  use 'terrortylor/nvim-comment'
+  use '9mm/vim-closer'
+  use {
+    'kyazdani42/nvim-tree.lua',
+    requires = 'kyazdani42/nvim-web-devicons'
+  }
+
+  use 'ishan9299/nvim-solarized-lua'
+  use {
+    'hoob3rt/lualine.nvim',
+    requires = {'kyazdani42/nvim-web-devicons', opt = true}
+  }
+  use {
+    'tanvirtin/vgit.nvim', requires = { 'nvim-lua/plenary.nvim' }
+  }
+
+  require('vgit').setup()
+  require('nvim_comment').setup()
+  require('nvim-tree').setup()
+
+  require('lualine').setup({
+    options = {
+      theme = 'solarized_light',
+      section_separators = {'', ''},
+      component_separators = {'', ''}
+    }
+  })
+
+  -- LSP, etc
+  -- use 'hrsh7th/nvim-compe'
+  use 'neovim/nvim-lspconfig'
+
+  use 'nvim-lua/popup.nvim'
+  use 'nvim-telescope/telescope.nvim'
+end)
