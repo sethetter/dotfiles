a0c7e18fafd7b5bcb01659fa2200deef58186b28
Seth Etter <mail@sethetter.com>
2022-08-24 13:41:15 -0500

    nvim: set default formatters

--- vim/astronvim/init.lua
+++ vim/astronvim/init.lua
@@ -53,9 +53,11 @@ local config = {
       -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
       config.sources = {
         -- Set a formatter
-        null_ls.builtins.formatting.rufo,
-        -- Set a linter
-        null_ls.builtins.diagnostics.rubocop,
+        null_ls.builtins.formatting.stylua,
+        null_ls.builtins.formatting.shfmt,
+        null_ls.builtins.formatting.gofmt,
+        null_ls.builtins.formatting.goimports,
+        null_ls.builtins.formatting.prettier,
       }
       -- set up null-ls's on_attach function
       config.on_attach = function(client)
