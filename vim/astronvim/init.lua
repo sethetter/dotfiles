16227f1f44b5ec4170f6f552307b4a5f99c75c0b
Seth Etter <mail@sethetter.com>
2022-10-03 21:43:05 -0500

    astronvim: a bunch of stuff

--- vim/astronvim/init.lua
+++ vim/astronvim/init.lua
@@ -1,19 +1,16 @@
-local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 local config = {
-
 	-- Configure AstroNvim updates
 	updater = {
 		remote = "origin", -- remote to use
 		channel = "nightly", -- "stable" or "nightly"
 		version = "latest", -- "latest", tag name, or regex search like "v1.*" to only do updates before v2 (STABLE ONLY)
-		branch = "main", -- branch name (NIGHTLY ONLY)
+		branch = "nightly", -- branch name (NIGHTLY ONLY)
 		commit = nil, -- commit hash (NIGHTLY ONLY)
 		pin_plugins = nil, -- nil, true, false (nil will pin plugins on stable only)
 		skip_prompts = false, -- skip prompts about breaking changes
 		show_changelog = true, -- show the changelog after performing an update
 	},
 
-	-- Set colorscheme
 	colorscheme = "solarized",
 
 	-- set vim options here (vim.<first_key>.<second_key> =  value)
@@ -46,36 +43,6 @@ local config = {
 			{ "MaxMEllon/vim-jsx-pretty" },
 			{ "pantharshit00/vim-prisma" },
 		},
-		-- All other entries override the setup() call for default plugins
-		["null-ls"] = function(config)
-			local null_ls = require("null-ls")
-			-- Check supported formatters and linters
-			-- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/formatting
-			-- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
-			config.sources = {
-				-- Set a formatter
-				null_ls.builtins.formatting.stylua,
-				null_ls.builtins.formatting.shfmt,
-				null_ls.builtins.formatting.gofmt,
-				null_ls.builtins.formatting.goimports,
-				null_ls.builtins.formatting.prettier,
-			}
-			-- set up null-ls's on_attach function
-			config.on_attach = function(client)
-				-- NOTE: You can remove this on attach function to disable format on save
-				-- if client.server_capabilities.documentFormattingProvider then
-				-- 	vim.api.nvim_create_autocmd("BufWritePre", {
-				-- 		desc = "Auto format before save",
-				-- 		pattern = "<buffer>",
-				-- 		callback = vim.lsp.buf.formatting_sync,
-				-- 	})
-				-- end
-			end
-			return config -- return final config table
-		end,
-		treesitter = {
-			ensure_installed = { "lua" },
-		},
 		["nvim-lsp-installer"] = {
 			ensure_installed = {
 				"stylua",
@@ -106,27 +73,6 @@ local config = {
 		},
 	},
 
-	-- Modify which-key registration
-	["which-key"] = {
-		-- Add bindings
-		register_mappings = {
-			-- first key is the mode, n == normal mode
-			n = {
-				-- second key is the prefix, <leader> prefixes
-				["<leader>"] = {
-					-- which-key registration table for normal mode, leader prefix
-					-- ["N"] = { "<cmd>tabnew<cr>", "New Buffer" },
-				},
-			},
-		},
-	},
-
-	-- CMP Source Priorities
-	-- modify here the priorities of default cmp sources
-	-- higher value == higher priority
-	-- The value can also be set to a boolean for disabling default sources:
-	-- false == disabled
-	-- true == 1000
 	cmp = {
 		source_priority = {
 			nvim_lsp = 1000,
@@ -136,78 +82,30 @@ local config = {
 		},
 	},
 
-	-- Extend LSP configuration
-	lsp = {
-		on_attach = function(client, bufnr)
-			-- For lang servers that already have formatting set up with
-			-- null-ls, disable their local formatting.
-			if client.supports_method("textDocument/formatting") then
-				vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-				vim.api.nvim_create_autocmd("BufWritePre", {
-					group = augroup,
-					buffer = bufnr,
-					callback = function()
-						vim.lsp.buf.format({
-							filter = function(c)
-								-- apply whatever logic you want (in this example, we'll only use null-ls)
-								return c.name == "null-ls"
-							end,
-							bufnr = bufnr,
-						})
-					end,
-				})
-			end
-		end,
-
-		-- override the lsp installer server-registration function
-		-- server_registration = function(server, opts)
-		--   require("lspconfig")[server].setup(opts)
-		-- end,
-
-		-- Add overrides for LSP server settings, the keys are the name of the server
-		["server-settings"] = {
-			-- example for addings schemas to yamlls
-			-- yamlls = {
-			--   settings = {
-			--     yaml = {
-			--       schemas = {
-			--         ["http://json.schemastore.org/github-workflow"] = ".github/workflows/*.{yml,yaml}",
-			--         ["http://json.schemastore.org/github-action"] = ".github/action.{yml,yaml}",
-			--         ["http://json.schemastore.org/ansible-stable-2.9"] = "roles/tasks/*.{yml,yaml}",
-			--       },
-			--     },
-			--   },
-			-- },
-		},
-	},
-
-	-- Diagnostics configuration (for vim.diagnostics.config({}))
 	diagnostics = {
 		virtual_text = true,
 		underline = true,
 	},
 
+	mappings = {
+		n = {
+			[";"] = { ":", desc = "you know" },
+			["<leader>fo"] = { ":Goyo<CR>", desc = "Focus with Goyo" },
+			["<leader>t"] = { ":Neotree left toggle<CR>", desc = "" },
+			["<leader>e"] = { ":Neotree float toggle<CR>", desc = "Show neotree as float" },
+			["<leader>ft"] = { ":Neotree reveal_file=%<CR>", desc = "Reveal file in Neotree" },
+			["<leader>w/"] = { ":vsp<CR>", desc = "Split vertical" },
+			["<leader>w?"] = { ":sp<CR>", desc = "Split horizontal" },
+			["<leader>go"] = {
+				':exe "!gbrowse " . fnamemodify(expand("%"), ":~:.")<CR>',
+				desc = "Open in github / gitlab",
+			},
+		},
+	},
+
 	-- This function is run last
 	-- good place to configure mappings and vim options
 	polish = function()
-		-- Set key bindings
-		vim.keymap.set("n", ";", ":")
-
-		vim.keymap.set("n", "<leader>fo", ":Goyo<CR>")
-
-		vim.keymap.set("n", "<leader>t", ":Neotree left toggle<CR>")
-		vim.keymap.set("n", "<leader>e", ":Neotree float toggle<CR>")
-		vim.keymap.set("n", "<leader>ft", ":Neotree reveal_file=%<CR>")
-
-		vim.keymap.set("n", "<leader>w/", ":vsp<CR>")
-		vim.keymap.set("n", "<leader>w?", ":sp<CR>")
-
-		vim.keymap.set("n", "<leader>go", ':exe "!gbrowse " . fnamemodify(expand("%"), ":~:.")<CR>')
-
-		vim.keymap.set("n", "<leader>gg", function()
-			astronvim.toggle_term_cmd("lazygit -ucf '/Users/sethetter/Library/Application Support/lazygit/nvim.yml'")
-		end)
-
 		-- Set autocommands
 		vim.api.nvim_create_augroup("packer_conf", { clear = true })
 		vim.api.nvim_create_autocmd("BufWritePost", {
