6f09bc7cd71f526d5715bd2a9f9a1b96bdbc1ab2
Seth Etter <sethetter@gmail.com>
2023-09-06 09:01:34 -0500

    some lvim keybindings

--- vim/lvim/config.lua
+++ vim/lvim/config.lua
@@ -2,9 +2,21 @@
 -- Video Tutorials: https://www.youtube.com/watch?v=sFA9kX-Ud_c&list=PLhoH5vyxr6QqGu0i7tt_XoVK9v-KvZ3m6
 -- Forum: https://www.reddit.com/r/lunarvim/
 -- Discord: https://discord.com/invite/Xb9B4Ny
+
+-- Plugins
 lvim.plugins = {
   { "ishan9299/nvim-solarized-lua" },
 }
 
-lvim.colorscheme = "solarized"
+-- Display
+lvim.colorscheme = "solarized-flat"
 vim.opt.background = "light"
+
+-- Opts
+lvim.format_on_save.enabled = true
+
+-- Keybindings
+lvim.keys.normal_mode["<S-h>"] = ":bprev<CR>"
+lvim.keys.normal_mode["<S-l>"] = ":bnext<CR>"
+lvim.builtin.which_key.mappings["w?"] = { ":sp<cr>", "Split horizontal" }
+lvim.builtin.which_key.mappings["w/"] = { ":vsp<cr>", "Split vertical" }
