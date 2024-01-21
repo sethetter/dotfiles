5e75b4c1ad3b9d80bbc71b9875beba43df4e521f
Seth Etter <mail@sethetter.com>
2024-01-20 22:36:16 -0600

    run theme sync every 3 seconds

--- scripts/sys-theme.sh
+++ scripts/sys-theme.sh
@@ -1,19 +1,26 @@
 #!/usr/bin/env bash
 
-alacritty_config_file=$HOME/.config/alacritty/alacritty.toml
-alacritty_theme_file=$HOME/.config/alacritty/theme.toml
+function sync_theme() {
+  local alacritty_config_file=$HOME/.config/alacritty/alacritty.toml
+  local alacritty_theme_file=$HOME/.config/alacritty/theme.toml
 
-if [[ $(defaults read -g AppleInterfaceStyle 2> /dev/null) == "Dark" ]]
-then
-  echo "dark" > $HOME/.theme
+  if [[ $(defaults read -g AppleInterfaceStyle 2> /dev/null) == "Dark" ]]
+  then
+    echo "dark" > $HOME/.theme
 
-  rm $alacritty_theme_file && \
-    ln -s $HOME/.config/alacritty/themes/themes/everforest_dark.toml $alacritty_theme_file
-  touch $alacritty_config_file
-else
-  echo "light" > $HOME/.theme
+    rm $alacritty_theme_file && \
+      ln -s $HOME/.config/alacritty/themes/themes/everforest_dark.toml $alacritty_theme_file
+    touch $alacritty_config_file
+  else
+    echo "light" > $HOME/.theme
 
-  rm $alacritty_theme_file && \
-    ln -s $HOME/.config/alacritty/themes/themes/everforest_light.toml $alacritty_theme_file
-  touch $alacritty_config_file
-fi
+    rm $alacritty_theme_file && \
+      ln -s $HOME/.config/alacritty/themes/themes/everforest_light.toml $alacritty_theme_file
+    touch $alacritty_config_file
+  fi
+}
+
+for i in {1..20}; do
+  sync_theme
+  sleep 3
+done
