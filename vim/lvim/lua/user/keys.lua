ddc0c643315acf85bbd78f4d735c35b951448ed0
Seth Etter <mail@sethetter.com>
2024-02-26 07:30:38 -0600

    lvim: copilot in lua, formatting, etc

--- vim/lvim/lua/user/keys.lua
+++ vim/lvim/lua/user/keys.lua
@@ -1,20 +1,21 @@
 -- File navigation
-lvim.keys.normal_mode["\\"] = ":Neotree position=left reveal<cr>"
-lvim.keys.normal_mode["E"] = ":Neotree position=current reveal<cr>"
-lvim.builtin.which_key.mappings["e"] = {
+lvim.keys.normal_mode["\\"]           = ":Neotree position=left reveal<cr>"
+lvim.keys.normal_mode["E"]            = ":Neotree position=current reveal<cr>"
+lvim.builtin.which_key.mappings["e"]  = {
   ":Neotree position=left toggle<cr>",
-  "Toggle Neotree in current position",
+  "Toggle Neotree",
   mode = { "n" }
 }
+lvim.builtin.which_key.mappings["o"]  = { ":Outline<cr>", "Toggle outline sidebar" }
 
 -- Buffer cycling
-lvim.keys.normal_mode["<S-h>"] = ":bprev<cr>"
-lvim.keys.normal_mode["<S-l>"] = ":bnext<cr>"
-lvim.builtin.which_key.mappings["CC"] = { ":bufdo bd<cr>", "Close all buffers", mode = { "n" } }
+lvim.keys.normal_mode["<S-h>"]        = ":bprev<cr>"
+lvim.keys.normal_mode["<S-l>"]        = ":bnext<cr>"
+-- lvim.builtin.which_key.mappings["CC"] = { ":bufdo bd<cr>", "Close all buffers", mode = { "n" } }
 
 -- Tab management
-lvim.keys.normal_mode["<C-n>"] = ":tabnext<cr>"
-lvim.keys.normal_mode["<C-p>"] = ":tabprev<cr>"
+lvim.keys.normal_mode["<C-n>"]        = ":tabnext<cr>"
+lvim.keys.normal_mode["<C-p>"]        = ":tabprev<cr>"
 lvim.builtin.which_key.mappings["tn"] = { ":tabnew<cr>", "New tab", mode = { "n" } }
 lvim.builtin.which_key.mappings["td"] = { ":tabclose<cr>", "Close tab", mode = { "n" } }
 
@@ -26,20 +27,31 @@ lvim.builtin.which_key.mappings["ts"] = { ":lua SyncTheme()<cr>", "Sync system t
 -- lvim.keys.normal_mode["<M-h>"] = "3zh"
 -- lvim.keys.normal_mode["<M-l>"] = "3zl"
 
+-- Splits
 lvim.builtin.which_key.mappings["v?"] = { ":sp<cr>", "Split horizontal", mode = { "n" } }
 lvim.builtin.which_key.mappings["v/"] = { ":vsp<cr>", "Split vertical", mode = { "n" } }
 
-lvim.builtin.which_key.mappings["Z"] = { ":ZenMode<cr>", "Zen mode", mode = { "n" } }
+-- Writing mode
+lvim.builtin.which_key.mappings["Z"]  = { ":ZenMode<cr>", "Zen mode", mode = { "n" } }
 
-lvim.builtin.which_key.mappings["Q"] = { ":qa<cr>", "Quit all" }
-lvim.builtin.which_key.mappings["X"] = { ":qa!<cr>", "Quit all (force)" }
+-- Quit
+lvim.builtin.which_key.mappings["Q"]  = { ":qa<cr>", "Quit all" }
+lvim.builtin.which_key.mappings["X"]  = { ":qa!<cr>", "Quit all (force)" }
 
+-- Copilot
+lvim.builtin.which_key.mappings["C"]  = {
+  ":lua require('copilot.panel').open()<cr>",
+  "Copilot panel open",
+  mode = { "n" },
+}
+
+-- AI
 lvim.builtin.which_key.mappings["aa"] = { ":AI ", "AI complete text" }
 lvim.builtin.which_key.mappings["ae"] = { ":AIEdit ", "AI edit text" }
 lvim.builtin.which_key.mappings["ac"] = { ":AIChat ", "AI chat" }
 lvim.builtin.which_key.mappings["ar"] = { ":AIRedo<cr>", "Redo last AI command" }
 
-lvim.builtin.which_key.mappings["o"] = { ":Outline<cr>", "Toggle outline sidebar" }
+
 
 -- Git stuff!
 lvim.builtin.which_key.mappings["gh"] = {
--- vim/lvim/lua/user/plugins/copilot.lua
+++ vim/lvim/lua/user/plugins/copilot.lua
@@ -1,16 +1,33 @@
+-- return {
+--   "zbirenbaum/copilot-cmp",
+--   event = "InsertEnter",
+--   dependencies = { "zbirenbaum/copilot.lua" },
+--   config = function()
+--     vim.defer_fn(function()
+--       -- https://github.com/zbirenbaum/copilot.lua/blob/master/README.md#setup-and-configuration
+--       require("copilot").setup({
+--         suggestion = { enabled = false },
+--         panel = { enabled = false },
+--       })
+--       -- https://github.com/zbirenbaum/copilot-cmp/blob/master/README.md#configuration
+--       require("copilot_cmp").setup()
+--     end, 100)
+--   end,
+-- }
 return {
-  "zbirenbaum/copilot-cmp",
+  "zbirenbaum/copilot.lua",
+  cmd = "Copilot",
   event = "InsertEnter",
-  dependencies = { "zbirenbaum/copilot.lua" },
   config = function()
-    vim.defer_fn(function()
-      -- https://github.com/zbirenbaum/copilot.lua/blob/master/README.md#setup-and-configuration
-      require("copilot").setup({
-        suggestion = { enabled = false },
-        panel = { enabled = false },
-      })
-      -- https://github.com/zbirenbaum/copilot-cmp/blob/master/README.md#configuration
-      require("copilot_cmp").setup()
-    end, 100)
+    require("copilot").setup({
+      suggestion = {
+        enabled = true,
+        auto_trigger = true,
+        debounce = 75,
+        keymap = {
+          accept = "<M-CR>",
+        },
+      },
+    })
   end,
 }
