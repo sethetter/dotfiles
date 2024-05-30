7a42307326fa122f22b9baf31fa8141c9f28748e
Seth Etter <mail@sethetter.com>
2024-05-30 07:18:43 -0500

    chore(lvim): remove some commented code

--- vim/lvim/lua/user/plugins/copilot.lua
+++ vim/lvim/lua/user/plugins/copilot.lua
@@ -1,27 +1,3 @@
--- return {
---   "zbirenbaum/copilot-cmp",
---   event = "InsertEnter",
---   dependencies = { "zbirenbaum/copilot.lua" },
---   config = function()
---     vim.defer_fn(function()
---       -- https://github.com/zbirenbaum/copilot.lua/blob/master/README.md#setup-and-configuration
---       require("copilot").setup({
---         suggestion = { enabled = false },
---         panel = { enabled = false },
---       })
---       -- https://github.com/zbirenbaum/copilot-cmp/blob/master/README.md#configuration
---       require("copilot_cmp").setup()
---     end, 100)
---   end,
--- }
--- return {
---   "zbirenbaum/copilot.lua",
---   cmd = "Copilot",
---   event = "InsertEnter",
---   config = function()
---     require("copilot").setup()
---   end,
--- }
 return {
   "github/copilot.vim",
 }
