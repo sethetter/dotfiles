15ca11a7095e867a2a3ec324fb7d0827631a1034
Seth Etter <mail@sethetter.com>
2024-11-03 07:29:09 -0600

    nvim tree floating mode

--- nvim/lua/plugins/nvim-tree.lua
+++ nvim/lua/plugins/nvim-tree.lua
@@ -1,10 +1,40 @@
 return {
-    "nvim-tree/nvim-tree.lua",
-    version = "*",
-    lazy = false,
-    config = function()
-        vim.g.loaded_netrw = 1
-        vim.g.loaded_netrwPlugin = 1
-        require("nvim-tree").setup({})
-    end,
+  "nvim-tree/nvim-tree.lua",
+  version = "*",
+  lazy = false,
+  config = function()
+    vim.g.loaded_netrw = 1
+    vim.g.loaded_netrwPlugin = 1
+    require("nvim-tree").setup({
+      hijack_netrw = true,
+      disable_netrw = true,
+      hijack_directories = {
+        enable = true
+      },
+      view = {
+        float = {
+          enable = true,
+          open_win_config = function()
+            local screen_w = vim.opt.columns:get()
+            local screen_h = vim.opt.lines:get() - vim.opt.cmdheight:get()
+            local window_w = screen_w - 4
+            local window_h = screen_h - 4
+            local window_w_int = math.floor(window_w)
+            local window_h_int = math.floor(window_h)
+            local center_x = (screen_w - window_w_int) / 2
+            local center_y = ((vim.opt.lines:get() - window_h_int) / 2)
+                - vim.opt.cmdheight:get()
+            return {
+              border = 'rounded',
+              relative = 'editor',
+              row = center_y,
+              col = center_x,
+              width = window_w_int,
+              height = window_h_int,
+            }
+          end,
+        },
+      },
+    })
+  end,
 }
