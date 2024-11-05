b9a14abeb8647c8889d9c82787aa187ad48dba2c
Seth Etter <mail@sethetter.com>
2024-11-05 08:53:43 -0600

    simpler local configs

---
+++ nvim/lua/plugins/config-local.lua
@@ -0,0 +1,17 @@
+return {
+  "klen/nvim-config-local",
+  config = function()
+    require('config-local').setup({
+      -- Config file patterns to load (lua supported)
+      config_files = { "_seth/nvim.lua", ".nvim.lua", ".nvimrc", ".exrc" },
+
+      -- Where the plugin keeps files data
+      hashfile = vim.fn.stdpath("data") .. "/config-local",
+
+      autocommands_create = true, -- Create autocommands (VimEnter, DirectoryChanged)
+      commands_create = true,     -- Create commands (ConfigLocalSource, ConfigLocalEdit, ConfigLocalTrust, ConfigLocalIgnore)
+      silent = false,             -- Disable plugin messages (Config loaded/ignored)
+      lookup_parents = false,     -- Lookup config files in parent directories
+    })
+  end
+}
--- nvim/lua/plugins/nvim-lspconfig.lua
+++ nvim/lua/plugins/nvim-lspconfig.lua
@@ -1,17 +1,22 @@
 return {
-  "neovim/nvim-lspconfig",
-  dependencies = {
-    "williamboman/mason.nvim",
-    "williamboman/mason-lspconfig.nvim",
-    "folke/neoconf.nvim",
+  {
+    "neovim/nvim-lspconfig",
+    dependencies = {
+      "williamboman/mason.nvim",
+    },
+    config = function()
+      local lspc = require("lspconfig")
+      local lspconfig_defaults = lspc.util.default_config
+      lspconfig_defaults.capabilities = vim.tbl_deep_extend(
+        "force",
+        lspconfig_defaults.capabilities,
+        require("cmp_nvim_lsp").default_capabilities()
+      )
+      lspc.lua_ls.setup({})
+    end,
   },
-  config = function()
-    local lspc = require("lspconfig")
-    local lspconfig_defaults = lspc.util.default_config
-    lspconfig_defaults.capabilities = vim.tbl_deep_extend(
-      "force",
-      lspconfig_defaults.capabilities,
-      require("cmp_nvim_lsp").default_capabilities()
-    )
-  end,
+  {
+    "williamboman/mason.nvim",
+    opts = {},
+  }
 }
