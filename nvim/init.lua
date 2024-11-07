bd9e7fdb74c32314407d9cd412fe81c044d33bf2
Seth Etter <mail@sethetter.com>
2024-11-07 11:20:17 -0600

    nvim cmp settings

--- nvim/init.lua
+++ nvim/init.lua
@@ -14,23 +14,8 @@ vim.opt.wrap = false
 -- Reserve a space in the gutter
 vim.opt.signcolumn = "yes"
 
-local cmp = require("cmp")
-
-cmp.setup({
-  sources = {
-    { name = "nvim_lsp" },
-  },
-  snippet = {
-    expand = function(args)
-      -- You need Neovim v0.10 to use vim.snippet
-      vim.snippet.expand(args.body)
-    end,
-  },
-  mapping = cmp.mapping.preset.insert({}),
-})
-
 vim.api.nvim_create_autocmd({ "FileType" }, {
-  pattern = ",markdown,gitlab,avante",
+  pattern = { "markdown", "gitlab", "avante", "" },
   callback = function()
     vim.opt_local.wrap = true
     vim.opt_local.linebreak = true
@@ -39,9 +24,9 @@ vim.api.nvim_create_autocmd({ "FileType" }, {
 
 -- TODO: Turn this into a convenience function that you can
 -- enable or disable on a per-project and per-filetype basis.
-vim.cmd([[
-augroup FormatAutogroup
-  autocmd!
-  autocmd BufWritePost * FormatWrite
-augroup END
-]])
+vim.api.nvim_create_augroup("FormatAutogroup", { clear = true })
+vim.api.nvim_create_autocmd("BufWritePost", {
+  group = "FormatAutogroup",
+  pattern = "*",
+  command = "FormatWrite",
+})
--- nvim/lua/plugins/cmp.lua
+++ nvim/lua/plugins/cmp.lua
@@ -1,7 +1,22 @@
 return {
   {
     "hrsh7th/nvim-cmp",
-    opts = {},
+    config = function()
+      local cmp = require("cmp")
+      cmp.setup({
+        sources = {
+          { name = "nvim_lsp" },
+          { name = "buffer" },
+          { name = "git" },
+        },
+        snippet = {
+          expand = function(args)
+            vim.snippet.expand(args.body)
+          end,
+        },
+        mapping = cmp.mapping.preset.insert({}),
+      })
+    end,
   },
   {
     "hrsh7th/cmp-nvim-lsp",
@@ -9,6 +24,11 @@ return {
       "neovim/nvim-lspconfig",
       "hrsh7th/nvim-cmp",
     },
+    opts = {},
+  },
+  {
+    "petertriho/cmp-git",
+    dependencies = { "hrsh7th/nvim-cmp" },
     opts = {},
   },
 }
