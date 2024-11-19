7cbd481dac1702b49f92d73ef3ed16fb18a0dfe3
Seth Etter <mail@sethetter.com>
2024-11-18 20:53:40 -0600

    only use lsp in cmp

--- nvim/lua/plugins/cmp.lua
+++ nvim/lua/plugins/cmp.lua
@@ -6,8 +6,6 @@ return {
       cmp.setup({
         sources = {
           { name = "nvim_lsp" },
-          { name = "buffer" },
-          { name = "git" },
         },
         snippet = {
           expand = function(args)
@@ -24,11 +22,6 @@ return {
       "neovim/nvim-lspconfig",
       "hrsh7th/nvim-cmp",
     },
-    opts = {},
-  },
-  {
-    "petertriho/cmp-git",
-    dependencies = { "hrsh7th/nvim-cmp" },
     opts = {},
   },
 }
