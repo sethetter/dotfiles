d9a1b35481b3bca10996305c1e9f1e1d57175ea0
Seth Etter <mail@sethetter.com>
2024-11-20 08:53:05 -0600

    enter selects completion

--- nvim/lua/plugins/cmp.lua
+++ nvim/lua/plugins/cmp.lua
@@ -12,7 +12,9 @@ return {
             vim.snippet.expand(args.body)
           end,
         },
-        mapping = cmp.mapping.preset.insert({}),
+        mapping = cmp.mapping.preset.insert({
+          ["<CR>"] = cmp.mapping.confirm({ select = true }),
+        }),
       })
     end,
   },
