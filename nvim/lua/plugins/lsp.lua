069dc35ab6aeebad4847bff611606108047d4c70
Seth Etter <mail@sethetter.com>
2024-12-09 07:40:58 -0600

    lsp file operations

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -105,4 +105,14 @@ return {
   {
     "prisma/vim-prisma",
   },
+  {
+    "antosha417/nvim-lsp-file-operations",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-tree.lua",
+    },
+    config = function()
+      require("lsp-file-operations").setup()
+    end,
+  },
 }
