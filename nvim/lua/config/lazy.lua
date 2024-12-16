9585532cc476f313645736e321caba5c342b8bff
Seth Etter <mail@sethetter.com>
2024-12-16 10:20:13 -0600

    disable lazy.nvim auto checking on boot
    so nvim starts up faster!

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -30,6 +30,6 @@ require("lazy").setup({
   -- Configure any other settings here. See the documentation for more details.
   -- colorscheme that will be used when installing plugins.
   install = { colorscheme = { "rose-pine" } },
-  -- automatically check for plugin updates
-  checker = { enabled = true },
+  -- disable automatic checking for plugin updates
+  checker = { enabled = false },
 })
