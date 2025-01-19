f67bb663cfaa9154baf748e1f8d4d1e7ddcd23e4
Seth <seth@starlab.etter.haus>
2025-01-19 14:17:35 -0600

    default to rose pine dawn

--- nvim/lua/plugins/colors.lua
+++ nvim/lua/plugins/colors.lua
@@ -4,9 +4,10 @@ return {
     name = "rose-pine",
     config = function()
       require("rose-pine").setup({
-        variant = "main",
+        variant = "dawn",
       })
-      vim.cmd("colorscheme rose-pine")
+      vim.api.nvim_set_option_value("background", "light", {})
+      vim.cmd("colorscheme rose-pine-dawn")
     end,
   },
   {
@@ -34,8 +35,8 @@ return {
     opts = {
       update_interval = 1000,
       set_dark_mode = function()
-        vim.api.nvim_set_option_value("background", "dark", {})
-        vim.cmd("colorscheme rose-pine-main")
+        -- vim.api.nvim_set_option_value("background", "dark", {})
+        -- vim.cmd("colorscheme rose-pine-main")
       end,
       set_light_mode = function()
         vim.api.nvim_set_option_value("background", "light", {})
