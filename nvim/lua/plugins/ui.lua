e4d6de422a3c4e0d7ba3e932c3fdf9fc371e5e3d
Seth Etter <mail@sethetter.com>
2025-01-20 21:41:19 -0600

    don't hide gitignored files

--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -12,6 +12,7 @@ return {
       filesystem = {
         filtered_items = {
           hide_dotfiles = false,
+          hide_gitignored = false,
         },
         window = {
           mappings = {
