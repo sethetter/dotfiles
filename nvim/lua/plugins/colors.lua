faf831c3547ef42bcf87071187f006d26d78cd8e
Seth Etter <mail@sethetter.com>
2025-02-03 20:42:12 -0600

    rose pine moon

--- nvim/lua/plugins/colors.lua
+++ nvim/lua/plugins/colors.lua
@@ -34,7 +34,7 @@ return {
       update_interval = 1000,
       set_dark_mode = function()
         vim.api.nvim_set_option_value("background", "dark", {})
-        vim.cmd("colorscheme rose-pine-main")
+        vim.cmd("colorscheme rose-pine-moon")
       end,
       set_light_mode = function()
         vim.api.nvim_set_option_value("background", "light", {})
